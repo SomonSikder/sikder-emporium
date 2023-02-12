212a653588df3c17acb2e28dd6c2c0e79f5111ce
SomonSikder <somonsikder777@gmail.com>
2023-02-12 14:11:25 +0600

    Product Pagenation

--- back-end/controller/productController.js
+++ back-end/controller/productController.js
@@ -35,10 +35,25 @@ const getAllProducts = asyncHandler(async (req, res) => {
       query = query.sort("-createdAt");
     }
 
-    // Sorting
+    // Limit
     if (req.query.fields) {
       const fields = req.query.fields.split(",").join(" ");
       query = query.select(fields);
+    } else {
+      query = query.select("-__v");
+    }
+
+    // Pagination
+    if (req.query.page) {
+      const page = req.query.page;
+      const limit = req.query.limit;
+      const skip = (page - 1) * limit;
+      query = query.skip(skip).limit(limit);
+
+      if (req.query.page) {
+        const productCount = await Product.countDocuments();
+        if (skip >= productCount) throw new Error("This page dose not exists");
+      }
     } else {
       query = query.select("-__v");
     }
